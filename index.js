963761247148fb7c41aef85bf7f9f3c3abd0b539
343dev <343dev@users.noreply.github.com>
2020-03-17 12:55:21 +0700

    Small refactoring

--- index.js
+++ index.js
@@ -9,17 +9,24 @@ const prepareFilePaths = require('./lib/prepareFilePaths');
 
 async function optimizt({ paths, webp, lossless, verbose }) {
   const spinner = ora('Processing. Please wait...').start();
-  const pathsList = prepareFilePaths(paths);
-
-  const params = { spinner, paths: pathsList, lossless, verbose };
 
   if (webp) {
-    await createWebp(params);
+    await createWebp({
+      paths: prepareFilePaths(paths, ['gif', 'jpeg', 'jpg', 'png']),
+      spinner,
+      lossless,
+      verbose,
+    });
   } else {
-    await optimize(params);
+    await optimize({
+      paths: prepareFilePaths(paths, ['gif', 'jpeg', 'jpg', 'png', 'svg']),
+      spinner,
+      lossless,
+      verbose,
+    });
   }
 
-  spinner.stop();
+  spinner.stopAndPersist({ text: 'Done!' });
 }
 
 module.exports = optimizt;
