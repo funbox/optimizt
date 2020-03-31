8a242d4a413d8fc71b1c93fe36d86003d02998d5
343dev <343dev@users.noreply.github.com>
2020-03-31 16:40:37 +0500

    Refactor spinner logic

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const ora = require('ora');
-
 const createWebp = require('./lib/createWebp');
 const { enableVerbose } = require('./lib/log');
 const optimize = require('./lib/optimize');
 const prepareFilePaths = require('./lib/prepareFilePaths');
+const spinner = require('./lib/spinner');
 
 async function optimizt({ paths, webp, lossless, verbose }) {
-  const spinner = ora('Processing. Please wait...').start();
+  spinner.start();
 
   if (verbose) enableVerbose();
 
   if (webp) {
     await createWebp({
       paths: prepareFilePaths(paths, ['gif', 'jpeg', 'jpg', 'png']),
-      spinner,
       lossless,
     });
   } else {
     await optimize({
       paths: prepareFilePaths(paths, ['gif', 'jpeg', 'jpg', 'png', 'svg']),
-      spinner,
       lossless,
     });
   }
