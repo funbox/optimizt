47934076e5220986a3ec1054b1685ede758af5f9
343dev <343dev@users.noreply.github.com>
2022-04-08 03:47:09 +0500

    Add showTotal.test.js

---
+++ tests-jest/showTotal.test.js
@@ -0,0 +1,23 @@
+const showTotal = require('../lib/showTotal');
+
+test('Savings size and compression ratio are displayed', () => {
+  const fileSize = 1048576;
+
+  console.log = jest.fn();
+
+  showTotal(fileSize, fileSize / 2);
+  expect(console.log.mock.calls[0][1]).toBe('Yay! You saved 512 KB (50%)');
+
+  console.log.mockRestore();
+});
+
+test('Savings size and compression ratio are not displayed', () => {
+  const fileSize = 1048576;
+
+  console.log = jest.fn();
+
+  showTotal(fileSize, fileSize * 2);
+  expect(console.log.mock.calls[0][1]).toBe('Done!');
+
+  console.log.mockRestore();
+});
